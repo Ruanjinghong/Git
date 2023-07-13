f0c1efd821f746105f25607acbbba1eef1ae98e0
Ruanjinghong <1942806641@qq.com>
2023-07-13 16:27:35 +0800

    修改aaaa

--- Gitt/src/main/java/com/Ruan/Git/Test/Test.java
+++ Gitt/src/main/java/com/Ruan/Git/Test/Test.java
@@ -1,5 +1,7 @@
 package com.Ruan.Git.Test;
 
+import java.sql.SQLOutput;
+
 /**
  * @Author:阮景鸿 Description:TODO
  * @DateTime: 2023/7/13 16:07
@@ -7,6 +9,7 @@ package com.Ruan.Git.Test;
 public class Test {
    public static void main(String[] args) {
       System.out.println("欢迎来到git");
+      System.out.println("aaaaaa");
    }
 
 }
