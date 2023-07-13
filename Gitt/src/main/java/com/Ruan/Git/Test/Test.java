693dd9f2b6b05d94776e7a8809a4461bfbbe9521
Ruanjinghong <1942806641@qq.com>
2023-07-13 16:14:44 +0800

    创建一个工程gitt

---
+++ Gitt/src/main/java/com/Ruan/Git/Test/Test.java
@@ -0,0 +1,12 @@
+package com.Ruan.Git.Test;
+
+/**
+ * @Author:阮景鸿 Description:TODO
+ * @DateTime: 2023/7/13 16:07
+ **/
+public class Test {
+   public static void main(String[] args) {
+      System.out.println("欢迎来到git");
+   }
+
+}
